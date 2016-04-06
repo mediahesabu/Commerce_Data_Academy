023f3808d6852c12d6213e6fdd52f6953ff49375
Randy Carlson <fcarlson@ieee.org>
2016-04-06 13:30:33 -0400

    Added files via upload

---
+++ function.py
@@ -0,0 +1,16 @@
+
+def hello():
+
+ print 'hello world'
+ print 'hello world again' + ' add me'
+ print 'hello world' [1] + 'hello world' [0]
+ print len('Hello World')
+
+def pi():
+ pi=3.14
+ text = 'The value of pi is ' + str(pi)
+ print text
+
+hello()
+
+pi()
